6b406cd1facb05591f45638bf47608778c81dea5
furaha707 <zxcvyein950823@gmail.com>
2023-11-03 09:04:48 +0900

    feat: 스터딩

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,6 @@ import { DefaultTheme } from "styled-components"
 
 export const darkTheme: DefaultTheme = {
   bgColor: "#3F8CF2",
-  textColor: "white",
   boardColor: "#DADFE9",
   cardColor: "white",
 }
